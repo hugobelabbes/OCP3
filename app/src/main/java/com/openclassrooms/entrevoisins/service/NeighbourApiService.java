c3070bd42709e5b9fbef16f1acb3b5003e27eb3e
Hugo Belabbes <hugo.belabbes@gmail.com>
2023-06-14 15:30:10 +0200

    14/06/2023 Ajout des commentaires dans le NeighbourApiService.java

--- app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/service/NeighbourApiService.java
@@ -28,12 +28,23 @@ public interface NeighbourApiService {
      */
     void createNeighbour(Neighbour neighbour);
 
-    //Rajouter commentaire ici basé sur le même modèle que ci dessus
+    /**
+     * Get my Neighbour id
+     * @param id
+     */
     Neighbour getByID(Long id);
 
-    //Récupérer liste des users favo
+    /**
+     * Get all my Favorites Neighbours
+     * @return {@link List}
+     */
     List<Neighbour> getFavorites();
 
+    /**
+     * Set a neighbour as favorit or not
+     * @param id
+     * @param isFavorit
+     */
     void setIsFavoritNeighbour(Long id, Boolean isFavorit);
 
 
